2f33c226991e7a7cfc7952a78bff3ce15ff35621
erastov-alex <erastov.alex@gmail.com>
2023-10-02 14:24:06 +0300

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://sysu-hcp.net/lip/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 5421248
+PREVIEW_IMAGE_ID: int = 5448956
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/cihp"
