f54c81ae1f454d84aea6bf0150be29fc134931c8
erastov-alex <erastov.alex@gmail.com>
2023-10-02 15:41:20 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "CIHP"
 PROJECT_NAME_FULL: str = "CIHP: Crowd Instance-level Human Parsing"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
